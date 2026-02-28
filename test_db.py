6dabf2d6d501c6a005551e8e7d982b951937e0d4
RASTY26 <shahrasty99@gamil.com>
2026-02-28 21:40:00 +0545

    db

--- test_db.py
+++ test_db.py
@@ -1,7 +1,8 @@
 # test_db.py
 
 from db.database import init_database
-
+  
+  # this is database 
 if __name__ == "__main__":
     print("🚀 Initializing ORA Jewelry Database...")
     init_database()
